9135e33668aec8dd1281c6c415c79221df1ad838
FinixSolar <tempfinix2026@gmail.com>
2026-06-29 15:21:04 +0530

    remove some error

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const client = createClient(
-  process.env.local.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.local.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  NEXT_PUBLIC_SUPABASE_URL,
 );
 
 export default client;
